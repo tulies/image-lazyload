4f2af89e05af6ce675231b5e2ab73f190872684a
王嘉炀 <346461062@qq.com>
2019-09-25 18:00:55 +0800

    init

---
+++ build/webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path')
+const webpack = require('webpack')
+// 清除文件
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+// const merge = require('webpack-merge')
+// const isDev = process.env.NODE_ENV === 'development'
+
+const config = {
+  mode: 'development', // production  development
+  watch: true,
+  watchOptions: {
+    poll: 1000, // 监测修改的时间(ms)
+    aggregateTimeout: 500, // 防止重复按键，500毫米内算按键一次
+    ignored: ['../node_modules/', '../dist/'] // 不监测
+  },
+  resolve: {
+    extensions: ['.js'],
+    // 配置别名，在项目中可缩减引用路径
+    alias: {
+      '@': path.resolve('src')
+    }
+  },
+  entry: {
+    app: path.join(__dirname, '../src/index.js')
+  },
+  output: {
+    filename: 'image-lazyload.js',
+    path: path.join(__dirname, '../dist')
+    // publicPath: 'http://127.0.0.1:8000/public/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/,
+        enforce: 'pre' // 预处理，之前先做校验
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  // devTool: '#cheap-modsule-eval-source-map',  // webpack4中貌似不需要
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    // new webpack.NoEmitOnErrorsPlugin()  // 4.x中取消了
+    // 删除文件 保留新文件
+    new CleanWebpackPlugin({
+      dry: false,
+      verbose: true
+    })
+  ]
+}
+// config = merge(config, devConfig)
+module.exports = config
+
+// 参考这个
+// https://www.cnblogs.com/goloving/p/8657205.html
---
+++ src/index.js
@@ -0,0 +1,25 @@
+class ImageLazyLoad {
+  constructor (el, options) {
+    this.el = el
+    this.elements = document.querySelectorAll(this.el)
+    const settings = {
+      threshold: 0,
+      // failure_limit   : 0,
+      event: 'scroll',
+      // effect          : "show",
+      container: window,
+      data_attribute: 'original',
+      skip_invisible: false,
+      appear: null,
+      load: null,
+      placeholder: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAANSURBVBhXYzh8+PB/AAffA0nNPuCLAAAAAElFTkSuQmCC'
+    }
+    console.log(settings)
+    // console.log(456)
+
+    // this.settings = Object.assign(settings, options)
+    // this.loadStart.apply(this)
+  }
+}
+
+export default ImageLazyLoad
